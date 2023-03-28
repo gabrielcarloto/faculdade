12184ea92e8e28b694c2e35ddb82877954b26e0a
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-03-28 20:41:17 -0300

    refatora as implementações para dentro da classe List

--- Estruturas de Dados/Trabalho 1/List.h
+++ Estruturas de Dados/Trabalho 1/List.h
@@ -14,14 +14,36 @@ template <typename T> class List : public BaseList<T> {
   Node *lastNode = NULL;
 
 public:
-  List(const T &array, const size_t length);
-  List(const size_t length = 0);
+  List(const T &array, const size_t length) {
+    this->length = length;
+
+    for (const T item : array) {
+      push(item);
+    }
+  };
+
+  List(const size_t length = 0) { this->length = length; };
+
   ~List();
 
   T &operator[](size_t index) override;
 
   T &at(intmax_t index) override;
-  void push(const T &item) override;
+
+  void push(const T &item) override {
+    Node *node = new Node;
+
+    if (firstNode == NULL)
+      firstNode = node;
+    else
+      lastNode->next = node;
+
+    node->prev = lastNode;
+    lastNode = node;
+
+    node->data = item;
+  };
+
   void remove(size_t index) override;
   void insert(T item, size_t index = 0) override;
   void replace(T item, size_t index = 0) override;
@@ -30,29 +52,3 @@ public:
   bool find(ItemIndexCallback<T, bool> filterFn, T &item) override;
   List<T> &filter(ItemIndexCallback<T, bool> filterFn) override;
 };
-
-template <typename T> List<T>::List(const T &array, const size_t length) {
-  this->length = length;
-
-  for (const T item : array) {
-    push(item);
-  }
-}
-
-template <typename T> List<T>::List(const size_t length) {
-  this->length = length;
-}
-
-template <typename T> void List<T>::push(const T &item) {
-  Node *node = new Node;
-
-  if (firstNode == NULL)
-    firstNode = node;
-  else
-    lastNode->next = node;
-
-  node->prev = lastNode;
-  lastNode = node;
-
-  node->data = item;
-}
