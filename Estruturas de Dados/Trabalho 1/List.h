4124e5343e132706784e5c2a2b999304f3d25460
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-03-28 11:44:00 -0300

    adiciona declaração de List

---
+++ Estruturas de Dados/Trabalho 1/List.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "BaseList.h"
+#include <iostream>
+
+template <typename T> class List : public BaseList<T> {
+  struct Node {
+    T data;
+    Node *next = NULL;
+  };
+
+  size_t currentIndex = 0;
+  T *firstNode = NULL;
+  T *lastNode = NULL;
+
+  void commonConstructor(size_t length);
+
+public:
+  List(const T &array, const size_t length);
+  List(const size_t length = 0);
+  ~List();
+
+  T &operator[](size_t index) override;
+
+  T &at(intmax_t index) override;
+  void push(const T &item) override;
+  void remove(size_t index) override;
+  void insert(T item, size_t index = 0) override;
+  void replace(T item, size_t index = 0) override;
+  void forEach(ItemIndexCallback<T> callback, size_t startIndex = 0) override;
+  bool findIndex(ItemIndexCallback<T, bool> filterFn, size_t &index) override;
+  bool find(ItemIndexCallback<T, bool> filterFn, T &item) override;
+  List<T> &filter(ItemIndexCallback<T, bool> filterFn) override;
+};
