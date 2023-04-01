01bab05aec27b7b5c6c9a1f2e092e99a643b50ea
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-04-01 17:09:23 -0300

    adiciona expectEqual

--- Estruturas de Dados/Trabalho 1/test.h
+++ Estruturas de Dados/Trabalho 1/test.h
@@ -1,6 +1,8 @@
 #pragma once
+#include "utils.h"
 #include <functional>
 #include <iostream>
+#include <sstream>
 #include <stdexcept>
 
 using TestCallback = const std::function<void()> &;
@@ -8,6 +10,14 @@ using TestCallback = const std::function<void()> &;
 #define assert(condition)                                                      \
   if (!(condition))                                                            \
   throw std::logic_error("Assertion failed: " #condition)
+
+#define expectEqual(lhs, rhs)                                                  \
+  if (lhs != rhs) {                                                            \
+    std::ostringstream stringStream;                                           \
+    stringStream << "Expected " << lhs << " (exp: " #lhs ") to be equal to "   \
+                 << rhs << " (exp: " #rhs ")\n";                               \
+    throw std::logic_error(stringStream.str());                                \
+  }
 
 #define expectThrow(fn, ...)                                                   \
   try {                                                                        \
