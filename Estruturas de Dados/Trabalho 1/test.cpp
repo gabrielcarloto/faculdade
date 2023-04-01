ac7db38a2008602db26ba23b281d5fcf22a5c32a
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-04-01 11:27:06 -0300

    altera a formatação dos testes

--- Estruturas de Dados/Trabalho 1/test.cpp
+++ Estruturas de Dados/Trabalho 1/test.cpp
@@ -1,19 +1,33 @@
 #include "test.h"
 #include <exception>
+#include <string.h>
+
+#define BG_GREEN "\033[30;42m"
+#define BG_RED "\033[30;41m"
+#define BG_YELLOW "\033[30;43m"
+#define RESET_COLOR "\033[39;49m"
+
+#define TEST_SUCCESS BG_GREEN " PASS " RESET_COLOR
+#define TEST_SUITE_TEXT " TEST "
+#define TEST_SUITE BG_YELLOW TEST_SUITE_TEXT RESET_COLOR
+#define TEST_ERROR BG_RED " FAIL " RESET_COLOR
 
 void test::describe(std::string description, TestCallback fn) {
-  std::cout << description << "\n";
+  std::cout << TEST_SUITE << " " << description << "\n";
   fn();
 }
 
 void test::it(std::string description, TestCallback fn) {
+  const int testSuiteLength = strlen(TEST_SUITE_TEXT);
+  const std::string tab = std::string(testSuiteLength, ' ');
+
   try {
     fn();
-    std::cout << "    " << TEST_SUCCESS << " " << description << "\n";
+    std::cout << tab << TEST_SUCCESS << " " << description << "\n";
   } catch (...) {
     std::exception_ptr e = std::current_exception();
 
-    std::cout << "    " << TEST_ERROR << " " << description << "\n\n";
+    std::cout << tab << TEST_ERROR << " " << description << "\n\n";
     std::rethrow_exception(e);
   }
 }
--- Estruturas de Dados/Trabalho 1/test.h
+++ Estruturas de Dados/Trabalho 1/test.h
@@ -5,9 +5,6 @@
 
 using TestCallback = const std::function<void()> &;
 
-#define TEST_SUCCESS "\033[32mPASS\033[39m"
-#define TEST_ERROR "\033[31mFAIL\033[39m"
-
 #define assert(condition)                                                      \
   if (!(condition))                                                            \
   throw std::logic_error("Assertion failed: " #condition)
